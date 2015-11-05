00f58b9eeedc75a0e0d4e8cf069208c1bec6e829
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-05 17:56:32 -0500

    got signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,5 +39,15 @@ class UsersController < ApplicationController
       flash[:alert] = "There was a problem deleting the user."   
     end   
     redirect_to :back
-  end
+  	end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+      params.require(:user).permit(:fname, :lname, :email, :password, :phone)
+    end
 end
