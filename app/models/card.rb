63da0f945a282b3c986de78943fb3531ddae23d7
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-04 22:50:06 -0500

    added possible validation for card number

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,6 @@
 class Card < ActiveRecord::Base
   has_many :cards_users
   has_many :users, through: :cards_users
+    #  message: "invalid card number" }
+	  #  validates :card_number, format: { with: /\A(3[4,7][0-9]{13}|4([0-9]{12}|[0-9]{15})|6011[0-9]{12}|65[0-9]{14}|5[1-5]{1}[0-9]{14}|\b622(1(2[6-9][0-9]{10}|[3-9][0-9]{11})|[2-8][0-9]{12}|9([01][0-9]{11}|2[0-5][0-9]{10}))\b)\z/,
 end
