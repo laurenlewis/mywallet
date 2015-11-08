251042256f5c8cc7da60bdca8c8ad5f70132797d
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-07 22:23:31 -0500

    adding new cards to user and getting them to show on user's profile

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,35 @@
 class CardsController < ApplicationController
+  def new
+  	@card = Card.new
+  end
+  
+  def create 
+    @card = Card.new(card_params)
+
+    respond_to do |format|
+      if @card.save
+        format.html { redirect_to @card, notice: 'Card was successfully added.' }
+        format.json { render :show, status: :created, location: @card }
+      else
+        format.html { render :new }
+        format.json { render json: @card.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def index 
+    @cards = Card.all  
+    @users = User.all
+  end 
+
+  def show
+  	@card = Card.find(params[:id])
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def card_params
+      params.require(:card).permit(:card_number, :card_type, :expiration_month, :expiration_year)
+  end  
+
 end
