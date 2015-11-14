28c6efa9d64aae12fc314287bcc7e7dd36e75382
Lauren Lewis <laurenlewis1.618@gmail.com>
2015-11-14 15:16:48 -0500

    added email uniqueness validation to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :cards_users, :class_name => 'CardsUser'
   has_many :cards, through: :cards_users
 
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :password, presence: true, length: { minimum: 4,
     too_short: "must have at least 4 characters"}
   #validates phone? 
